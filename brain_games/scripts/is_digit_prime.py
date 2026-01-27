6c1e5d27d030542a9b315212858d0ce1dfc82e11
Maria Kulikova <maryku4815@gmail.com>
2026-01-27 14:08:53 +0300

    Implement Is Digit Prime game

---
+++ brain_games/scripts/is_digit_prime.py
@@ -0,0 +1,72 @@
+"""Модуль содержит игру 'Is Digit Prime?'."""
+
+from secrets import choice
+
+import prompt  # type: ignore[import-untyped]
+
+from brain_games.cli import logger, welcome_user
+
+_WRONG_ANSWERS_MAP = {
+    'yes': 'no',
+    'no': 'yes',
+}
+
+
+def is_digit_prime(number: int) -> bool:
+    """Проверяет, простое ли число, и сравнивает результат с ответом игрока.
+
+    Args:
+        number (int): Число, которое проверяется.
+
+    Returns:
+        bool: True, если число простое, иначе False.
+
+    """
+    dividers = [2, 3, 5]
+
+    if number in dividers:
+        return True
+
+    # Если хоть одно число делится на предложенный делитель - оно не простое
+    for divider in dividers:
+        if number % divider == 0:
+            return False
+
+    return True
+
+
+def main() -> None:
+    """Точка входа игры 'Is Digit Prime?'."""
+    name = welcome_user()
+    if not name:
+        return
+
+    logger.info('Answer "yes" if given number is prime. Otherwise answer "no".')
+
+    for _try in range(3):
+        number = choice(range(2, 1000))
+        correct_answer = 'yes' if is_digit_prime(number) else 'no'
+
+        answer = prompt.string(f'Question: {number}')
+        logger.info('Your answer: %s', answer)
+
+        formatted_answer = answer.lower().strip()
+
+        # Если ответ правильный - игра продолжается
+        if formatted_answer == correct_answer:
+            logger.info('Correct!')
+        else:
+            correct_answer = _WRONG_ANSWERS_MAP[formatted_answer]
+
+            logger.info(
+                '"%s" is wrong answer ;(. Correct answer is "%s"'
+                "\n Let's try again, %s!",
+                formatted_answer, correct_answer, name,
+            )
+            return
+
+    logger.info('Congratulations, %s!', name)
+
+
+if __name__ == '__main__':
+    main()
