546dfe289442f413e566a0fb07bfc920cac9e224
Maria Kulikova <maryku4815@gmail.com>
2026-01-27 22:07:36 +0300

    Move Calculator game to games directory

--- brain_games/scripts/calculator.py
+++ brain_games/scripts/calculator.py
@@ -1,54 +1,32 @@
-"""Модуль содержит игру 'Calculator?'."""
+"""Модуль содержит функционал для игры 'Calculator'."""
 
 import operator
 from secrets import choice
 
-import prompt  # type: ignore[import-untyped]
-
-from brain_games.cli import get_two_any_numbers, logger, welcome_user
+from brain_games.cli import get_two_any_numbers
 
 
-def main() -> None:
-    """Точка входа игры 'Calculator'."""
-    name = welcome_user()
-    if not name:
-        return
+def get_main_data() -> tuple[str, str]:
+    """Подготавливает условие задачи и ответ для игры.
 
-    logger.info('What is the result of the expression?')
+    Returns:
+        tuple[str, str]:
+            str: условие задачи для вопроса игроку.
+            str: правильный ответ на вопрос.
 
-    for _try in range(3):
-        number_1, number_2 = get_two_any_numbers(1, 50)
+    """
+    number_1, number_2 = get_two_any_numbers(1, 50)
+    any_operator = choice(['+', '-', '*'])
 
-        # Выбираем случайный оператор
-        any_operator = choice(['+', '-', '*'])
+    # Словарь для сопоставления символа операции с функцией
+    operators = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+    }
 
-        # Словарь для сопоставления символа операции с функцией
-        operators = {
-            '+': operator.add,
-            '-': operator.sub,
-            '*': operator.mul,
-        }
+    math_function = operators[any_operator]
+    correct_answer = math_function(number_1, number_2)
 
-        math_function = operators[any_operator]
-        correct_answer = math_function(number_1, number_2)
-
-        expression = f'{number_1} {any_operator} {number_2}'
-
-        answer = prompt.string(f'Question: {expression} ')
-        logger.info('Your answer: %s', answer)
-
-        if correct_answer == int(answer):
-            logger.info('Correct!')
-        else:
-            logger.info(
-                "'%s' is wrong answer ;(. Correct answer is '%s'"
-                "\n Let's try again, %s!",
-                answer, correct_answer, name,
-            )
-            return
-
-    logger.info('Congratulations, %s!', name)
-
-
-if __name__ == '__main__':
-    main()
+    condition = f'{number_1} {any_operator} {number_2}'
+    return condition, correct_answer
