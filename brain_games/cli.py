0acea6183cd67793b56da4fc36afb85e7ae78121
Maria Kulikova <maryku4815@gmail.com>
2026-01-27 15:54:34 +0300

    Decrease ruff line-length

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -6,7 +6,8 @@ from secrets import choice
 import prompt  # type: ignore[import-untyped]
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
 )
 logger = logging.getLogger(__name__)
 
@@ -23,7 +24,9 @@ def welcome_user() -> str | None:
     logger.info('Welcome to the Brain Games!')
     name: str | None = prompt.string('May I have your name? ').strip()
     if not name:
-        logger.info("It's impossible to continue without your name. Maybe next time.")
+        logger.info(
+            "It's impossible to continue without your name. Maybe next time.",
+        )
         return None
 
     logger.info('Hello, %s!', name)
